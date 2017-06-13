5e2822a82dc5aaf2cacbf599067d0889e2d457d8
RootPermission <736410355@qq.com>
2017-06-13 13:04:12 +0800

    commit app

---
+++ app/src/main/java/com/app/lanmusic/LanMusicpplication.java
@@ -0,0 +1,10 @@
+package com.app.lanmusic;
+
+import android.app.Application;
+
+/**
+ * author：JiaXing on 2017/6/13 11:26
+ * e-mail：JiaXingGoo@gmail.com
+ */
+public class LanMusicpplication extends Application {
+}
---
+++ app/src/main/java/com/app/lanmusic/ui/MainActivity.java
@@ -0,0 +1,10 @@
+package com.app.lanmusic.ui;
+
+import android.app.Activity;
+
+/**
+ * author：JiaXing on 2017/6/13 11:25
+ * e-mail：JiaXingGoo@gmail.com
+ */
+public class MainActivity extends Activity {
+}
